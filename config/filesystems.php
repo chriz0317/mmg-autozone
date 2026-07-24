d77d0bbf6bcef650b074329a7333d99cdd984fe9
chriz0317 <lagonchristopher1@gmail.com>
2026-07-24 23:32:42 +0800

    Add fallback keys to Cloudinary config to prevent crashes

--- config/filesystems.php
+++ config/filesystems.php
@@ -50,6 +50,9 @@ return [
         'cloudinary' => [
             'driver' => 'cloudinary',
             'url' => env('CLOUDINARY_URL'),
+            'cloud' => env('CLOUDINARY_CLOUD_NAME', ''),
+            'key' => env('CLOUDINARY_API_KEY', ''),
+            'secret' => env('CLOUDINARY_API_SECRET', ''),
         ],
 
         's3' => [
